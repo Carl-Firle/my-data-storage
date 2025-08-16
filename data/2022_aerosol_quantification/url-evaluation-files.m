bfda6f1cddd6880a8a4599507e3eb912edef8ca8
Carl Firle <92682699+Carl-Firle@users.noreply.github.com>
2025-08-16 22:32:39 +0200

    Create url-evaluation-files.m

---
+++ data/2022_aerosol_quantification/url-evaluation-files.m
@@ -0,0 +1,171 @@
+{{"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/01/2023-03-07%2010.01.47.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/01/01_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/01/\
+QHA01%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/02/2023-03\
+-10%2009.47.59.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/02/02_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/02/QHA02%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/03/2023-03-10%2012.46.27.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/03/03_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/03/\
+QHA03%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/04/2023-03\
+-17%2016.23.29.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/04/04_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/04/QHA04%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/05/2023-03-17%2018.29.59.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/05/05_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/05/\
+QHA05%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/06/2023-03\
+-21%2009.18.10.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/06/06_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/06/QHA06%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/07/2023-03-21%2013.03.53.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/07/07_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/07/\
+QHA07%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/08/2023-03\
+-23%2009.23.06.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/08/08_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/08/QHA08%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/09/2023-03-23%2013.27.18.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/09/09_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/09/\
+QHA09%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/10/2023-03\
+-28%2013.24.24.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/10/10_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/10/QHA10%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/11/2023-03-29%2009.16.03.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/11/11_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/11/\
+QHA11%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/12/2023-03\
+-29%2013.33.25.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/12/12_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/12/QHA12%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/13/2023-04-03%2008.40.54.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/13/13_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/13/\
+QHA13%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/14/2023-04\
+-03%2010.52.04.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/14/14_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/14/QHA14%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/15/2023-04-04%2015.16.01.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/15/15_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/15/\
+QHA15%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/16/2023-04\
+-04%2017.53.08.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/16/16_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/16/QHA16%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/17/2023-04-05%2010.17.32.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/17/17_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/17/\
+QHA17%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/18/2023-04\
+-05%2012.24.27.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/18/18_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/18/QHA18%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/19/2023-04-06%2010.27.28.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/19/19_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/19/\
+QHA19%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/20/2023-04\
+-12%2009.33.55.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/20/20_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/20/QHA20%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/21/2023-04-12%2012.18.05.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/21/21_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/21/\
+QHA21%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/22/2023-04\
+-13%2013.26.25.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/22/22_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/22/QHA22%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/23/2023-04-13%2015.54.39.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/23/23_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/23/\
+QHA23%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/24/2023-04\
+-14%2010.15.38.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/24/24_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/24/QHA24%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/25/2023-04-26%2017.50.33.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/25/25_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/25/\
+QHA25%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/26/2023-04\
+-27%2014.03.15.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/26/26_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/26/QHA26%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/27/2023-05-02%2010.18.41.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/27/27_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/27/\
+QHA27%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/28/2023-05\
+-04%2013.40.59.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/28/28_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/28/QHA28%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/29/2023-05-05%2013.46.14.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/29/29_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/29/\
+QHA29%20Spiro%20und%20Diff.txt"}, {"https://github.com/Carl-Firle/my-data-sto\
+rage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/30/2023-06\
+-06%2009.06.49.xlsx", "https://github.com/Carl-Firle/my-data-storage/raw/refs\
+/heads/main/data/2022_aerosol_quantification/raw-data/30/30_protocol.txt", "h\
+ttps://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_ae\
+rosol_quantification/raw-data/30/QHA30%20Spiro%20und%20Diff.txt"}, 
+ {"https://github.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/202\
+2_aerosol_quantification/raw-data/31/2023-06-06%2018.20.52.xlsx", "https://gi\
+thub.com/Carl-Firle/my-data-storage/raw/refs/heads/main/data/2022_aerosol_qua\
+ntification/raw-data/31/31_protocol.txt", "https://github.com/Carl-Firle/my-d\
+ata-storage/raw/refs/heads/main/data/2022_aerosol_quantification/raw-data/31/\
+QHA31%20Spiro%20und%20Diff.txt"}}
